fb3b412fad971a759b16c937b766a8c27a6811a6
Ramnaresh Ulaganathan <121196326+ramnareeesh@users.noreply.github.com>
2023-09-04 23:29:34 +0530

    LinkedListCycle - Easy - Hashmap

---
+++ 141.LinkedListCycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head) -> bool:
+        visited = set()
+        current = head
+        while current:
+            if current in visited:
+                return True
+            visited.add(current)
+            current = current.next
+        return False
+
+
+
